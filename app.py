0f30e25fd21f1787c59c10ec45f70337b022f04e
jGYQrw gga <Kul.vova2009@yandex.ru>
2026-03-04 18:26:26 +0300

    remake app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 import os
 
+
 class Handler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
@@ -9,6 +10,7 @@ class Handler(BaseHTTPRequestHandler):
         app = os.environ.get('APP', 'LMS')
         message = f"Hello from {app}! 👋\nPath: {self.path}\n"
         self.wfile.write(message.encode())
+
 
 port = int(os.getenv('PORT', 8000))
 server = HTTPServer(('', port), Handler)
